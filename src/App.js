7ffa98476297119f3904e6c9a96ea32537ec4922
kartikmodi8347 <kartikmodi8347@gmail.com>
2023-05-23 09:46:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import ProductList from './pages/ProductList';
-import CartItem from './pages/CartItem';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import CartItem from "./pages/CartItem";
 
 function App() {
   return (
     <>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/login" Component={Login}/>
-    <Route path="/register" Component={Register}/>
-    <Route path="/product-list" Component={ProductList}/>
-    <Route path="/cart-item" Component={CartItem}/>
-    
-   </Routes>
-   </BrowserRouter>
-    </> 
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" Component={Register} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path="/product-list" Component={ProductList} />
+          <Route path="/cart-item" Component={CartItem} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
