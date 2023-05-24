a22f95e1bd7675b6bce19cded93116ebcebd95a8
kartikmodi8347 <kartikmodi8347@gmail.com>
2023-05-24 09:50:51 +0530

    to make Registration form with proper validation..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import '../styles/Header.css'
 import logo from '../assets/images/logo.jpg'
 import { Button } from '@mui/material'
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
       <img src={logo} />
       <div className="button">
-        <Button variant="text" color="primary">Login</Button>
-        <Button variant="text">Register</Button>
+      <Link to="/login"><Button variant="text" color="primary">Login</Button></Link>
+      <Link to="/register"><Button variant="text">Register</Button></Link>
         <Button variant="contained" endIcon={<AiOutlineShoppingCart />}>cart</Button>
       </div>
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,128 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+//import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/Register.css'
 import { TextField, Button } from '@mui/material'
 import { AiOutlineSearch } from 'react-icons/ai';
 
+const RegistrationForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
+  const validateForm = () => {
+    const errors = {};
+
+    //  for Validate FirstName using this formate
+    if (firstName.trim() === '') {
+      errors.firstName = 'First Name is required';
+    }
+
+    // for Validate LastName using this formate
+    if (lastName.trim() === '') {
+      errors.lastName = 'Last Name is required';
+    }
+
+    // for Validate EmailAddress using this formate
+    if (email.trim() === '') {
+      errors.email = 'Email Address is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Invalid Email Address';
+    }
+
+    //  for Validate Password must be this formate
+    if (password === '') {
+      errors.password = 'Password is required';
+    } else if (password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+
+    //  for Validate ConfirmPassword must be same as Password
+    if (confirmPassword === '') {
+      errors.confirmPassword = 'Confirm Password is required';
+    } else if (confirmPassword !== password) {
+      errors.confirmPassword = 'Passwords do not match';
+    }
+
+    setErrors(errors);
+
+    // Check if there are no errors
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      // Perform form submission or API call put API here
+      console.log('Form submitted!');
+    }
+  };
+
+  return (
+    <>
+    <div className= "Reg-form">
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {errors.firstName && <span>{errors.firstName}</span>}
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        {errors.lastName && <span>{errors.lastName}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {errors.email && <span>{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errors.password && <span>{errors.password}</span>}
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+   </>
+  );
+};
 
 function Register() {
   return (
@@ -24,9 +140,8 @@ function Register() {
         
         <p>Home : Create an Account for expirience</p>
         <h1>Login or Create an Account</h1><br />
-      
-        <Link to="/login"><Button variant="contained">Login</Button> <br /> <br /></Link>
-        </div>
+        <RegistrationForm />
+      </div>
       <Footer />
     </>
   )
