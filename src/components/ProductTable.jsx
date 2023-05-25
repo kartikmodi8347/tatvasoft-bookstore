2b4fdbd49561f7e49cd01da9b3c5f9a5ccaaa452
kartikmodi8347 <kartikmodi8347@gmail.com>
2023-05-25 12:05:10 +0530

    make a table

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,78 +1,158 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { sumBy } from "lodash";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+  root: {
+    backgroundColor: "white",
+    color: "#333333",
+    fontSize: "larger",
+    textAlign: "center",
+    border: 10,
   },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
+    border: 10,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
+  "&:last-child td, &:last-child th": {
+    border: 10,
   },
 }));
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+export default function ProductTable() {
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Product 1",
+      price: 110,
+      quantity: 0,
+      category: "Category 1",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 115,
+      quantity: 0,
+      category: "Category 2",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 256,
+      quantity: 0,
+      category: "Category 3",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 2476,
+      quantity: 0,
+      category: "Category 4",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 3025,
+      quantity: 0,
+      category: "Category 5",
+    },
+  ]);
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+  const handleIncrementQuantity = (id) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      )
+    );
+  };
 
-export default function CustomizedTables() {
+  const handleDecrementQuantity = (id) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === id && product.quantity > 0
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      )
+    );
+  };
+
+  const calculateTotalPrice = (price, quantity) => {
+    return price * quantity;
+  };
+
+  const handleCartButtonClick = () => {
+    const totalPrice = sumBy(products, (product) =>
+      calculateTotalPrice(product.price, product.quantity)
+    );
+    alert(`Total Price: ₹${totalPrice}`);
+  };
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ Width: 700 }} aria-label="product table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell>Price</StyledTableCell>
+              <StyledTableCell>Quantity</StyledTableCell>
+              <StyledTableCell>Category</StyledTableCell>
+              <StyledTableCell>Actions</StyledTableCell>
+              <StyledTableCell>Total Price</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <StyledTableRow key={product.id}>
+                <StyledTableCell>{product.name}</StyledTableCell>
+                <StyledTableCell>{product.price}</StyledTableCell>
+                <StyledTableCell>{product.quantity}</StyledTableCell>
+                <StyledTableCell>{product.category}</StyledTableCell>
+                <StyledTableCell>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => handleDecrementQuantity(product.id)}
+                    disabled={product.quantity === 0}
+                  >
+                    -
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => handleIncrementQuantity(product.id)}
+                  >
+                    +
+                  </Button>
+                </StyledTableCell>
+                <StyledTableCell>
+                  {calculateTotalPrice(product.price, product.quantity)}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleCartButtonClick}
+      >
+        Cart
+      </Button>
+    </div>
   );
 }
